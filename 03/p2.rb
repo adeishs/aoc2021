aab8b4d3e1f1cf2ffb4c974ea0e97565badd8dfa
ade ishs <ade@adeishs.com>
2021-12-03 21:11:29 +1100

    DRY

--- 03/p2.rb
+++ 03/p2.rb
@@ -1,33 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+def process(recs, mode)
+  rev_mode = (mode.to_i ^ 1).to_s
+  filtered_recs = recs
+  (0...recs[0].count).each do |i|
+    f = %w[0 1].map { |b| [b, filtered_recs.map { |rec| rec[i] }.count(b)] }
+               .to_h
+    b = f['0'] > f['1'] ? rev_mode : mode
+
+    filtered_recs = filtered_recs.select { |rec| rec[i] == b }
+    return filtered_recs[0].join('').to_i(2) if filtered_recs.count == 1
+  end
+end
+
 recs = $stdin.each_line.map(&:chomp).map { |line| line.split('') }
-
-line_len = recs[0].count
-filtered_recs = recs
-(0...line_len).each do |i|
-  bs = filtered_recs.map { |rec| rec[i] }
-  f = {}
-  f[0] = bs.count('0')
-  f[1] = bs.count('1')
-  b = f[0] > f[1] ? '0' : '1'
-
-  filtered_recs = filtered_recs.select { |rec| rec[i] == b }
-  break if filtered_recs.count == 1
-end
-o = filtered_recs[0].join('')
-
-filtered_recs = recs
-(0...line_len).each do |i|
-  bs = filtered_recs.map { |rec| rec[i] }
-  f = {}
-  f[0] = bs.count('0')
-  f[1] = bs.count('1')
-  b = f[0] > f[1] ? '1' : '0'
-
-  filtered_recs = filtered_recs.select { |rec| rec[i] == b }
-  break if filtered_recs.count == 1
-end
-co2 = filtered_recs[0].join('')
-
-puts o.to_i(2) * co2.to_i(2)
+puts process(recs, '1') * process(recs, '0')

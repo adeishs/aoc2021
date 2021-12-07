91eafc81b0e4e094f758cf6aafa0d3ed2ac4a8ec
ade ishs <ade@adeishs.com>
2021-12-07 13:02:24 +1100

    rubocop

--- 06/p1.rb
+++ 06/p1.rb
@@ -5,9 +5,10 @@ NEW_FISH_STATE = 8
 
 fish = $stdin.gets.chomp.split(',').map(&:to_i)
 
-(1..80).each do |day|
-  num_of_new_fish = fish.select { |f| f.zero? }.count
-  fish = fish.map { |f| f == 0 ? 6 : f - 1 } + Array.new(num_of_new_fish, NEW_FISH_STATE)
+(1..80).each do |_day|
+  num_of_new_fish = fish.select(&:zero?).count
+  fish = fish.map { |f| f.zero? ? 6 : f - 1 } +
+         Array.new(num_of_new_fish, NEW_FISH_STATE)
 end
 
 puts fish.count

c8d1853f671914bff3c49def84704946b0cdcba3
ade ishs <ade@adeishs.com>
2021-12-07 16:16:34 +1100

    Improve code

--- 06/p1.rb
+++ 06/p1.rb
@@ -2,12 +2,13 @@
 # frozen_string_literal: true
 
 NEW_FISH_STATE = 8
+OLD_FISH_RESET_STATE = 6
 
 fish = $stdin.gets.chomp.split(',').map(&:to_i)
 
-(1..80).each do |_day|
+80.times do
   num_of_new_fish = fish.select(&:zero?).count
-  fish = fish.map { |f| f.zero? ? 6 : f - 1 } +
+  fish = fish.map { |f| f.zero? ? OLD_FISH_RESET_STATE : f - 1 } +
          Array.new(num_of_new_fish, NEW_FISH_STATE)
 end
 

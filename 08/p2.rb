290ac4fdb1617f9b4acd1e580b09425e87aa8095
ade ishs <ade@adeishs.com>
2021-12-09 01:19:15 +1100

    Refactor

--- 08/p2.rb
+++ 08/p2.rb
@@ -1,48 +1,47 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+def find_idx(in_segs, subset)
+  in_segs.find_index { |c| c & subset == subset }
+end
+
 def decode(code)
   input, output = code.split(' | ')
   in_segs = input.split(' ')
-                 .map { |c| c.split('').sort }
+                 .map { |c| c.chars.sort }
                  .sort { |a, b| b.count <=> a.count }
   seg_map = {}
   rev_map = Array.new(10)
   [[9, 1], [8, 7], [7, 4], [0, 8]].each do |i, m|
-    chars = in_segs.delete_at(i)
-    seg_map[chars.join('')] = m
-    rev_map[m] = chars
+    rev_map[m] = in_segs.delete_at(i)
+    seg_map[rev_map[m].join] = m.to_s
   end
 
   [[4, 9], [7, 0]].each do |sub, sup|
-    i = in_segs.find_index { |c| c & rev_map[sub] == rev_map[sub] }
-    chars = in_segs.delete_at(i)
-    seg_map[chars.join('')] = sup
-    rev_map[sup] = chars
+    rev_map[sup] = in_segs.delete_at(find_idx(in_segs, rev_map[sub]))
+    seg_map[rev_map[sup].join] = sup.to_s
   end
 
-  chars = in_segs.delete_at(0)
-  seg_map[chars.join('')] = 6
-  rev_map[6] = chars
+  i = 6
+  rev_map[i] = in_segs.delete_at(0)
+  seg_map[rev_map[i].join] = i
 
   sub, sup = [7, 3]
-  i = in_segs.find_index { |c| c & rev_map[sub] == rev_map[sub] }
-  chars = in_segs.delete_at(i)
-  seg_map[chars.join('')] = sup
-  rev_map[sup] = chars
+  rev_map[sup] = in_segs.delete_at(find_idx(in_segs, rev_map[sub]))
+  seg_map[rev_map[sup].join] = sup.to_s
 
   sub, sup = [5, 6]
   i = in_segs.find_index { |c| c & rev_map[sup] == c }
-  chars = in_segs.delete_at(i)
-  seg_map[chars.join('')] = sub
+  seg_map[in_segs.delete_at(i).join] = sub.to_s
 
-  chars = in_segs.delete_at(0)
-  seg_map[chars.join('')] = 2
+  seg_map[in_segs[0].join] = '2'
 
   output.split(' ')
-        .map { |s| seg_map[s.split('').sort.join('')].to_s }
-        .join('')
+        .map { |s| seg_map[s.chars.sort.join] }
+        .join
         .to_i
 end
 
-puts $stdin.each_line.map { |line| decode(line.chomp) }.reduce(&:+)
+puts $stdin.each_line
+           .map { |line| decode(line.chomp) }
+           .reduce(&:+)

991257d99a8cbcfd0fc6ac647f7c3c88b6b8da53
ade ishs <ade@adeishs.com>
2021-12-10 16:24:10 +1100

    Add day 10 part 1

---
+++ 10/p1.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+EXPECTED = {
+  '(' => ')',
+  '[' => ']',
+  '{' => '}',
+  '<' => '>'
+}.freeze
+
+SCORE = {
+  ')' => 3,
+  ']' => 57,
+  '}' => 1197,
+  '>' => 25_137
+}.freeze
+
+def get_err_score(codes)
+  expecteds = []
+  codes.each do |c|
+    if EXPECTED.keys.any?(c)
+      expecteds.push(EXPECTED[c])
+    elsif c != expecteds.pop
+      return SCORE[c]
+    end
+  end
+
+  0
+end
+
+puts $stdin.each_line
+           .map { |line| get_err_score(line.chomp.split('')) }.reduce(:+)

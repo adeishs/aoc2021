017bbc5579c008ab9ff49eed0df455b85247fde6
ade ishs <ade@adeishs.com>
2021-12-15 20:10:08 +1100

    Add day 15 part 2

---
+++ 15/p2.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def in_range(x, y, risks)
+  x.between?(0, risks[0].count - 1) && y.between?(0, risks.count - 1)
+end
+
+def calc_min_cost(risks)
+  paths = [[0, 0, 0]]
+  costs = Array.new(risks.count) { Array.new(risks[0].count, 999_999_999) }
+
+  loop do
+    cost, x, y = paths.shift
+    return cost if x == risks[0].count - 1 && y == risks.count - 1
+
+    [
+      [x + 1, y],
+      [x, y + 1],
+      [x - 1, y],
+      [x, y - 1]
+    ].select { |nx, ny| in_range(nx, ny, risks) }
+      .each do |nx, ny|
+      new_cost = cost + risks[ny][nx]
+      if new_cost < costs[ny][nx]
+        costs[ny][nx] = new_cost
+        paths.push([new_cost, nx, ny])
+      end
+    end
+
+    paths.sort! { |a, b| a[0] <=> b[0] }
+  end
+end
+
+def incr(r, i)
+  (r + i) % 9 + 1
+end
+
+def expand_map(risks)
+  x_len = risks[0].count
+  (0...risks.count).each do |y|
+    (0..3).each do |x|
+      risks[y] += risks[y][0...x_len].map { |r| incr(r, x) }
+    end
+  end
+
+  y_len = risks.count
+  (0..3).each do |y|
+    risks[0...y_len].each do |cols|
+      risks.append(cols.map { |r| incr(r, y) })
+    end
+  end
+
+  risks
+end
+
+puts calc_min_cost(
+  expand_map($stdin.each_line.map { |line| line.chomp.chars.map(&:to_i) })
+)

a2cbae96f25d509d0dc897b34211aa60b19d0a24
ade ishs <ade@adeishs.com>
2021-12-21 17:52:45 +1100

    Use fewer magic numbers

--- 21/p1.rb
+++ 21/p1.rb
@@ -21,11 +21,11 @@ num_of_rolls = 0
 player = 1
 winner = nil
 until winner
-  rolls_sum = (i...i + 3).map { |d| DIE_VALS[d % 100] }.reduce(:+)
-  num_of_rolls += 3
-  i = (i + 3) % 100
+  rolls = (i...i + 3).map { |d| DIE_VALS[d % 100] }
+  num_of_rolls += rolls.count
+  i = (i + rolls.count) % 100
 
-  pos[player] = 1 + (pos[player] - 1 + rolls_sum) % 10
+  pos[player] = 1 + (pos[player] - 1 + rolls.reduce(:+)) % 10
   score[player] += pos[player]
 
   (1..2).each do |c|

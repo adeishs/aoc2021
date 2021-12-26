0b815527ae66391118e8ef62624cc78a583e5e1d
ade ishs <ade@adeishs.com>
2021-12-26 18:38:11 +1100

    Avoid global var

--- 24/p1.rb
+++ 24/p1.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'set'
+# adapted from https://topaz.github.io/paste/#XQAAAQCaBAAAAAAAAAA0m0pnuFI8c/fBNAn6x25rti77on4e8DYCelyI4Xj/SWO86l3MhYKSt/IwY1X8XDsPi6oUd359E/fP3WUCy+Hd0NBX3ScDH1UMDMoIn89DqtRJAkuU26H+bJQMhuQJZGvHfbRq+cNenkcVuZMyoJg2X38kr/tdzPWRs0R3nEQAYf3r0cXmSlac2aJH0P2sl7z4weDgKeKkKfE5swiQJ2MN12HwuoRR3LBTiJQjtT73JpWF+KtQBulka0/rhUSDOrztKM4biu1JoxqydIgyDfWupEKKtAiW75B1XW73P7TSQe8BI9O2T12ql8E/CBnsomkNwZLvIqQuyxA8lRBFyEb7T2Ofx8p8uaMPHbMv786Ho5P2KtCBwYdoX3z3fIV+cETYydTzjakKrUdUMq7dRV/kbM91elWwnwaxWByBhJS7jtOshbq8mO2W3BfCQ48WxEmwVrceIMSV2txALQlDxsy1lduebYKTzWCNl58cRdbnvICOxfoV9eofWAd6YP8WsL0eQUXcgzd8K1OW7qe67FH89TwqknLQ8KvTA3WKR1z4kubNHFXrvJLyFPO4Xqk5QhtviCMkBIiUic3Fvme1CyUV5V9g4XGkZpp95hCiTZFAtqVUvWvQARQNtGBuWv0mw9NB7qM940S7lQeCGHE95fH1UtwHYFdNrEuyPVdRYw6oCBD9+eh6PL+vkkN3dz3Axp2EUdlP/qwVQQ==
 
 MODEL_LEN = 14
 MODEL_NUMS = [9, 8, 7, 6, 5, 4, 3, 2, 1]
@@ -15,8 +15,7 @@ def get_const_sets(insts)
   const_sets
 end
 
-$levels = {}
-def build_deps(const_sets, i, zs)
+def build_deps(const_sets, i, zs, levels)
   sols = {}
   consts = const_sets[i]
 
@@ -38,26 +37,27 @@ def build_deps(const_sets, i, zs)
       end
     end
   end
-  $levels[i] = sols
+  levels[i] = sols
 
-  build_deps(const_sets, i - 1, sols.keys) if i > 0
+  build_deps(const_sets, i - 1, sols.keys, levels) if i > 0
 end
 
 def cmp_levels(a, b)
   b[0] == a[0] ? b[1] <=> a[1] : b[0] <=> a[0]
 end
 
-def solve(i, z, sol)
+def solve(i, z, sol, levels)
   return sol.join('') if i == MODEL_LEN
 
-  $levels[i][z].sort { |a, b| cmp_levels(a, b) }.each do |l|
+  levels[i][z].sort { |a, b| cmp_levels(a, b) }.each do |l|
     w, nz = l
-    ans = solve(i + 1, nz, (sol + [w]).flatten)
+    ans = solve(i + 1, nz, sol + [w], levels)
     return ans if ans
   end
 end
 
 consts = get_const_sets($stdin.each_line.map { |line| line.chomp.split(' ') })
 zs = [0]
-build_deps(consts, MODEL_LEN - 1, zs)
-puts solve(0, 0, [])
+levels = {}
+build_deps(consts, MODEL_LEN - 1, zs, levels)
+puts solve(0, 0, [], levels)
